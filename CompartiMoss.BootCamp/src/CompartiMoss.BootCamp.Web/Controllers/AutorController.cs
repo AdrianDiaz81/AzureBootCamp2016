3b3a0d6f59d820b9e040644b953a564c8b4835a5
Adrian Diaz Cervera <ardydiaz@hotmail.com>
2016-04-13 08:25:37 +0200

    Modify AppSettings add Controller Autor and Revista

---
+++ CompartiMoss.BootCamp/src/CompartiMoss.BootCamp.Web/Controllers/AutorController.cs
@@ -0,0 +1,33 @@
+﻿using CompartiMoss.BootCamp.Web.Services;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CompartiMoss.BootCamp.Web.Controllers
+{
+    public class AutorController : Controller
+    {
+        private IAutor _service;
+
+        public AutorController(IAutor service)
+        {
+            _service = service;
+        }
+
+        
+        public IActionResult Index()
+        {
+            var autorCollection = _service.GetAllAutor();
+            return View(autorCollection);
+        }
+
+        [HttpGet]
+        public IActionResult Detail(string name)
+        {
+            var result = _service.GetAutorByName(name);
+            return View(result);
+        }
+
+     
+    }
+}
---
+++ CompartiMoss.BootCamp/src/CompartiMoss.BootCamp.Web/Controllers/RevistaController.cs
@@ -0,0 +1,26 @@
+﻿using CompartiMoss.BootCamp.Web.Services;
+using Microsoft.AspNet.Mvc;
+
+namespace DotNet.CompartiMOSS.Controllers
+{
+    public class RevistaController : Controller
+    {
+        private IRevista _service;
+        public RevistaController(IRevista service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var revistaCollection = _service.GetRevistas();
+            return View(revistaCollection);
+        }
+
+        public IActionResult Detail(string name)
+        {
+            var revista = _service.GetRevistaByTitle(name);
+            return View(revista);
+        }
+    }
+}
